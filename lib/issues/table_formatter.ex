62bb73d0f09f9e5303ca900b5ba4c4397c8d9726
jinlee0 <wls4370@gmail.com>
2023-08-20 09:07:19 +0900

    refactor: remove useless

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -15,12 +15,9 @@ defmodule Issues.TableFormatter do
 
   def split_into_columns(rows, headers) do
     for header <- headers do
-      for row <- rows, do: printable(row[header])
+      for row <- rows, do: to_string(row[header])
     end
   end
-
-  def printable(str) when is_binary(str), do: str
-  def printable(str), do: to_string(str)
 
   def widths_of(columns) do
     for column <- columns, do: column |> Enum.map(&String.length/1) |> Enum.max()
